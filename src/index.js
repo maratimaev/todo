19da84ef043833eb425f15cb51c724d68b2b7ce9
Marat Imaev <imaevmarat@outlook.com>
2019-11-13 16:34:49 +0500

    Todo List init

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDom from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import AppHeader from './components/app-header';
+import TodoList from './components/todo-list';
+import SearchPanel from './components/search-panel';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const todoData =  [
+    {id: 1, label: 'Drink Coffee', important: false},
+    {id: 2, label: 'Make App', important: true}
+];
+
+const App = () => {
+    return (
+        <div>
+            <AppHeader/>
+            <SearchPanel/>
+            <TodoList todos={todoData}/>
+        </div>
+    )
+};
+
+ReactDom.render(<App/>, document.getElementById('root'));
+console.log('Hello, World!');
